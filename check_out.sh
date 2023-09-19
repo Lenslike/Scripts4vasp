4e59ee88d595033d84a1acda7a394d174b145c7e
Lenslike <75530219+Lenslike@users.noreply.github.com>
2023-09-19 16:09:48 +0800

    Create check_out.sh

---
+++ check_out.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash 
+kwc='reached' # Key Word for Convergence
+kwf='Voluntary' # Key word for job finish
+soc=$(grep $kwc OUTCAR |tail -n 1 |awk '{print $1}')
+sof=$(grep $kwf OUTCAR |tail -n 1 |awk '{print $1}')
+
+if [ $soc=$kwc ]; then 
+echo 'converged'
+fi
+
+if [ $sof=$kwf ]; then 
+echo 'finished'
+fi
+
+if [ $soc=$kwc -a $sof=$kwf ]; then 
+echo 'Perfect'
+fi
+(base) qli@lqlhz H2O_gas % bash check_out.sh 
+converged
+finished
+Perfect
