104f5a942cceea432f2fa3e5fe271e48e9050603
Lenslike <75530219+Lenslike@users.noreply.github.com>
2024-03-26 19:28:30 +0800

    Update submit4ph_cal.sh

--- submit4ph_cal.sh
+++ submit4ph_cal.sh
@@ -3,10 +3,10 @@
 # 获取当前脚本所在目录的绝对路径
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-for i in {1..8}; do
-    dir_name="disp-00$i"
+for((i=1; i<=60; i++)); do
+    dir_name="disp-$(printf "%03d" $i)"
     mkdir -p "$dir_name"
-    cp "POSCAR-00$i" "$dir_name/POSCAR"
+    cp "POSCAR-$(printf "%03d" $i)" "$dir_name/POSCAR"
     cp "POTCAR" "$dir_name"
     cp "INCAR" "$dir_name"
     cp "KPOINTS" "$dir_name"
